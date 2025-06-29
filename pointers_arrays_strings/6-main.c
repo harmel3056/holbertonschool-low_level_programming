571e0ab09054e80b0d2cd5086f2d93316f5eb011
harmel3056 <melh1988@hotmail.com>
2025-06-29 02:45:33 +0000

    Write a function that prints every other character of a string, starting with the first character, followed by a new line

---
+++ pointers_arrays_strings/6-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "Lorem ipsum dolor sit amet. In fugiat nihil aut beatae pariatur et optio omnis. Quo nihil alias et consequatur nihil non exercitationem quos et rerum illum nam expedita asperiores eum temporibus illo. Et dolores voluptatem eos nostrum voluptatem et incidunt autem qui sapiente maxime eos dolores nostrum qui omnis repellendus. In quas sint aut beatae distinctio At voluptatem sint cum optio mollitia vel aliquam optio hic voluptates sint eum quibusdam repellat";
+    puts2(str);
+
+    str = "Hello, World!";
+    puts2(str);
+
+    return (0);
+}
