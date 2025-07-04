17484a157d6672644e0aa5193e6ebb3494905eea
harmel3056 <melh1988@hotmail.com>
2025-07-04 09:28:21 +0000

    Write a function that returns the value of x raised to the power of y. Prototype: int _pow_recursion(int x, int y); If y is lower than 0, the function should return -1

--- recursion/main.h
+++ recursion/main.h
@@ -4,6 +4,7 @@
 int factorial(int n);
 int _putchar(char c);
 int _strlen_recursion(char *s);
+int _pow_recursion(int x, int y);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 
