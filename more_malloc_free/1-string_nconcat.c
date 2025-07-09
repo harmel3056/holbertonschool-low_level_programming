8a4f670fb037a244c4ac7144f6e167e832bc0d43
harmel3056 <melh1988@hotmail.com>
2025-07-09 03:07:43 +0000

    Write a function that concatenates two strings. Prototype: char *string_nconcat(char *s1, char *s2, unsigned int n); The returned pointer shall point to a newly allocated space in memory, which contains s1, followed by the first n bytes of s2, and null terminated. If the function fails, it should return NULL. If n is greater or equal to the length of s2 then use the entire string s2. if NULL is passed, treat it as an empty string

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string to copy
+ * @s2: second string to copy
+ * @n: number of bytes to copy from s2
+ *
+ * Description: concatenates two strings, returns NULL for failure
+ * Return: pointer in newly allocated space, containing s1 and s2[n]
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, length1, length2;
+	char *ptr;
+
+	if (s1 == NULL)
+		s1  = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[length1] != '\0')
+		length1++;
+
+	while (s2[length2] != '\0' && length2 < n)
+		length2++;
+
+	ptr = malloc((length1 + length2 + 1) * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < length1)
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+
+	j = 0;
+	while (j < length2)
+	{
+		ptr[i + j] = s2[j];
+		j++;
+	}
+
+	ptr[i + j] = '\0';
+
+	return (ptr);
+}
