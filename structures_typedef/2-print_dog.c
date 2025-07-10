b08937a2678228687f3adca21ca60a8760de4042
harmel3056 <melh1988@hotmail.com>
2025-07-10 09:37:54 +0000

    Write a function that prints a struct dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer
+ *
+ * Description: prints name, age, and owner of dog
+ * Return: void
+ */
+
+void print_dog(struct dog *d);
+{
+	if (d == NULL)
+		printf(""\n)?? got this formatting right?
+	my_dog		
+
+
+	if (name == NULL)
+		printf("nil); this has to be wrapped under the parent printf though
