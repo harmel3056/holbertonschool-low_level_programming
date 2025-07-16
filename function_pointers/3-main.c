b124f0179f7fa7f93644518aceac63b1a545f9cc
harmel3056 <melh1988@hotmail.com>
2025-07-16 06:48:28 +0000

    Basic calculator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -8,10 +8,18 @@
  * Return: void
  */
 
-void main( )
+int main(int argc, char *argv[])
 {
-	int (
-	int atoi(
+	int num1;
+	int num2;
+	int result;
+	int func;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	func = get_op_func(argv[2]);
+	result = func(num1, num2);
+	printf("%d", result);
 
 		*get_op_func selects correct function to perform operation asked by user, using a char
 		It calls back to op_functions.c, which contains the operations (such as a + b)
