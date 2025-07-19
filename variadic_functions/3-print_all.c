72f6bac08ba560391acb958297476188bd734ee7
harmel3056 <melh1988@hotmail.com>
2025-07-19 03:08:14 +0000

    Write a function that prints anything.

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -13,11 +13,11 @@
 void print_all(const char * const format, ...)
 {
 	 op_t ops[] = {
-        {"c", print_char},
-        {"i", print_integer},
-        {"f", print_float},
-        {"s", print_string},
-        {NULL, NULL}
+	{"c", print_char},
+	{"i", print_integer},
+	{"f", print_float},
+	{"s", print_string},
+	{NULL, NULL}
 	};
 	va_list list;
 	int i = 0;
@@ -25,51 +25,83 @@ void print_all(const char * const format, ...)
 	int printed_one = 0;
 
 	va_start(list, format);
-		while(format != NULL && format[i] != '\0')
+		while (format != NULL && format[i] != '\0')
 		{
 			while (j < 4)
 			{
-				if(*ops[j].t == format[i])
+				if (*ops[j].t == format[i])
 				{
 					if (printed_one == 1)
 						printf(", ");
-					
+
 					ops[j].f[&list];
 					printed_one = 1;
 				}
-				j = j++;
+				j++;
 			}
 			j = 0;
-			i = i++;
+			i++;
 		}
 		va_end(list);
 		printf("\n");
 }
 
+/**
+ * print_char - prints a character
+ * @list: va_list containing the character
+ *
+ * Return: void
+ */
+
 void print_char(va_list *list)
 {
 	char c;
+
 	c = va_arg(*list, char);
 	printf("%c", c);
 }
 
+/**
+ * print_integer - prints an integer
+ * @list: va_list containing the integer
+ *
+ * Return: void
+ */
+
 void print_integer(va_list *list)
 {
 	int i;
+
 	i = va_arg(*list, int);
 	printf("%i", i);
 }
 
+/**
+ * print_float - prints a float
+ * @list: va_list containing the float
+ *
+ * Return: void
+ */
+
 void print_float(va_list *list)
 {
 	float f;
+
 	f = va_arg(*list, float);
-	printf(%f, f);
+	printf("%f", f);
 }
+
+/**
+ * print_string - prints a string
+ * @list: va_list containing the string
+ *
+ * Return: void
+ */
 
 void print_string(va_list *list)
 {
 	char *s;
+
 	s = va_arg(*list, char *);
-	printf(%s, s);
+	printf("%s", s);
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
