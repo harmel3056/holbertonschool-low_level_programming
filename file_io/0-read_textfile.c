429807a9dbca5508750cf5d9069d1dc1d098f5fb
harmel3056 <melh1988@hotmail.com>
2025-09-09 09:00:57 +0000

    Function reads a text file and prints it to the POSIX stdout

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,16 +1,53 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include "main.h"
 
+/**
+ * read_textfile - Reads text to POSIX environment
+ * @filename: name of file to read
+ * @letters: number of letters to print
+ *
+ * Return: file is written and number of bytes written is returned
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    if(filename == NULL);
-        return (0);
-    
-    FILE *fp = fopen(filename, "r");
-    if(fp == NULL);
-    {
-        perror(0);
-        return (0);
-    }
-}   
+	int fd;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+	return (bytes_written);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -7,3 +7,4 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
+
